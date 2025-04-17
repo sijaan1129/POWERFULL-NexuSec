e11885d99dde8da40da7b04158ae545db2d89a30
sijaan1129 <sijaan53@gmail.com>
2025-04-18 00:40:38 +1200

    Update whitelist.py

--- cogs/whitelist.py
+++ cogs/whitelist.py
@@ -1,22 +1,20 @@
 import discord
 from discord.ext import commands
-from discord.ui import Select, View
 
 class Whitelist(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.whitelist = {}
+        self.whitelist = {}  # Store whitelisted users and roles
 
-    # Helper function to check if a user/role is whitelisted
     def is_whitelisted(self, guild_id, member_or_role):
+        """Check if user or role is whitelisted."""
         guild_whitelist = self.whitelist.get(guild_id, {"users": [], "roles": []})
         return member_or_role.id in guild_whitelist["users"] or any(role.id in guild_whitelist["roles"] for role in member_or_role.roles)
 
-    # Command to add a user or role to the whitelist
-    @commands.command(name="whitelist", help="Whitelist a user or role to bypass anti-spam/anti-link.")
+    @commands.command(name="whitelist", help="Whitelist a user or role.")
     async def whitelist(self, ctx, member_or_role: discord.abc.Snowflake):
+        """Add user or role to whitelist."""
         guild_id = ctx.guild.id
-        # Check if it's a user or a role
         if isinstance(member_or_role, discord.Member):
             self.whitelist.setdefault(guild_id, {"users": [], "roles": []})["users"].append(member_or_role.id)
             await ctx.send(f"{member_or_role} has been added to the whitelist.")
@@ -26,9 +24,9 @@ class Whitelist(commands.Cog):
         else:
             await ctx.send("Please provide a valid user or role.")
 
-    # Command to remove a user or role from the whitelist
     @commands.command(name="removewhitelist", help="Remove a user or role from the whitelist.")
     async def remove_whitelist(self, ctx, member_or_role: discord.abc.Snowflake):
+        """Remove user or role from whitelist."""
         guild_id = ctx.guild.id
         if isinstance(member_or_role, discord.Member):
             if member_or_role.id in self.whitelist.get(guild_id, {}).get("users", []):
@@ -45,9 +43,9 @@ class Whitelist(commands.Cog):
         else:
             await ctx.send("Please provide a valid user or role.")
 
-    # Command to view the current whitelist
-    @commands.command(name="viewwhitelist", help="View the current whitelist for users and roles.")
+    @commands.command(name="viewwhitelist", help="View the current whitelist.")
     async def view_whitelist(self, ctx):
+        """View the current whitelist for users and roles."""
         guild_id = ctx.guild.id
         guild_whitelist = self.whitelist.get(guild_id, {"users": [], "roles": []})
         users = [f"<@{user_id}>" for user_id in guild_whitelist["users"]]
